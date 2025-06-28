274003ce0e945114457191fa8bbfedadf15a1a3f
papa_hasuo <papa_hasuo@outlook.jp>
2025-06-28 21:58:56 +0900

    User→Personに変更

--- src/main/java/jp/ne/papapa/Main.java
+++ src/main/java/jp/ne/papapa/Main.java
@@ -9,20 +9,21 @@ import jp.ne.papapa.util.MyBatisUtil;
 
 public class Main {
     public static void main(String[] args) {
-        
         SqlSessionFactory sqlSessionFactory = MyBatisUtil.getSqlSessionFactory();
 
         try (SqlSession session = sqlSessionFactory.openSession()) {
-            PersonDao userDao = session.getMapper(PersonDao.class);
-            PersonDto user = userDao.getPersonById("00001"); // 例: ID=1のユーザー取得
-            if (user != null) {
-                System.out.println("Name: " + user.getName());
-                System.out.println("SexCd: " + user.getSex());
-                System.out.println("BirthDay: " + user.getBirthDay());
+            PersonDao personDao = session.getMapper(PersonDao.class);
+            PersonDto person = personDao.getPersonById("00001"); // 例: ID=1のユーザー取得
+            if (person != null) {
+                System.out.println("Name: " + person.getName());
+                System.out.println("Sex: " + person.getSex());
+                System.out.println("BirthDay: " + person.getBirthDay());
             } else {
                 System.out.println("ユーザーが見つかりませんでした。");
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("データ取得時にエラーが発生しました。");
         }
-
     }
 }
