ef88c978bf915eca82e16ddc1f21521ffeef7f81
papa_hasuo <papa_hasuo@outlook.jp>
2025-06-28 17:44:49 +0900

    Mybatis用Utilファイルの追加

---
+++ src/main/java/jp/ne/papapa/MyBatisUtil.java
@@ -0,0 +1,31 @@
+package jp.ne.papapa;
+
+import java.io.Reader;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+/**
+ * Mybatisを用いたデータベース接続のユーティリティクラス
+ */
+public class MyBatisUtil {
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        try {
+            Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        } catch (Exception e) {
+            throw new RuntimeException("Error initializing MyBatis: ", e);
+        }
+    }
+
+    /**
+     * SqlSessionFactoryを取得するメソッド
+     *
+     * @return SqlSessionFactoryのインスタンス
+     */
+    public static SqlSessionFactory getSqlSessionFactory() {
+        return sqlSessionFactory;
+    }
+}
